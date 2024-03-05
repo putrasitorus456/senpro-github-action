be58934a80a1206ea50da970ac26f77ac6a560f4
putrasitorus456 <putra.agung0304@mail.ugm.ac.id>
2024-03-05 08:20:57 +0700

    case 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
 
         <div>
           <h5 className={inter.className}>
-            [Putra Agung A.M. Sitorus] | [474928] 
+            [Putra Agung A.M. Sitorus] | [474928]
           </h5>
         </div>
       </main>
